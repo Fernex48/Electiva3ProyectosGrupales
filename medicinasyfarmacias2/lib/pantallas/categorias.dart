8b599b36ca74709068a23b3fabdf9f00c98b6888
Fernex48 <diegobejarano.adoc@gmail.com>
2021-12-01 23:50:49 -0600

    Version3

--- medicinasyfarmacias2/lib/pantallas/categorias.dart
+++ medicinasyfarmacias2/lib/pantallas/categorias.dart
@@ -1,9 +1,17 @@
+// POR AHORA NO HAY QUE HACER CASO A LOS IMPORTS QUE ESTAN COMENTADOS, QUE NO SE BORREN PERO TAMPOCO SE USEN
+
 // ignore_for_file: deprecated_member_use, camel_case_types
-
+//import 'package:firebase_core/firebase_core.dart';
+//import 'package:cloud_firestore/cloud_firestore.dart';
+//import 'package:firebase_auth/firebase_auth.dart';
+//import 'package:medicinasyfarmacias/pantallas/sucursales.dart';
+//import '../funciones/funciones.dart';
 import 'package:flutter/material.dart';
-import 'package:medicinasyfarmacias/pantallas/cat_seleccionada.dart';
-import '../funciones/funciones.dart';
+import 'package:medicinasyfarmacias/database/dbcategorias.dart';
 
+//El Stateful que conocemos, aquí se crea toda la metodología donde se interactúa con la base de datos.
+//Se necesita un contexto dentro del método donde se realizar la consulta a la base de datos, por eso fue más
+//conveniente hacer las consultas aquí
 class Categorias extends StatefulWidget {
   const Categorias({Key? key}) : super(key: key);
 
@@ -12,186 +20,87 @@ class Categorias extends StatefulWidget {
 }
 
 class _CategoriasState extends State<Categorias> {
+  //Elementos para lograr la conexión con la base de datos, usando el archivo dart "dbFarmacias"
+  //el cual tiene una clase llamada "DatabaseFarmacias", la cual contiene las funciones para realizar el CRUD.
+
+  //Objeto de la clase DatabaseFarmacias
+  late DatabaseCategorias db;
+  //Listado donde se irán guardando los registros de la colección consultada de la base.
+  List docs = [];
+  //Inicialización del objeto
+  initialise() {
+    db = DatabaseCategorias();
+    db.initiliase();
+    //Se hace uso de la función para leer todos los datos, se configura el estado internamente para asignar cada valor (registro de la colección)
+    //como elementos para la lista "docs"
+    //En otras palabras, guarda todos los registros de la colección Farmacias en el listado que luego se usará para mostrarlos en pantalla.
+    db.read().then((value) => {
+          setState(() {
+            docs = value;
+          })
+        });
+  }
+
+  //Se inicia el estado de la conexión...
+  @override
+  void initState() {
+    super.initState();
+    initialise();
+  }
+
+  //El widget principal, este contiene una barra de aplicación "AppBar" que a su vez, contendrá un botón para regresar, el título
+  //y una opción de bùsqueda con un ícono...
+  //este Widget contiene también el listado de las farmacias...
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: cuerpoApp(
-          tituloPantalla("Medicamentos"),
-          campoBuscar("Buscar producto"),
-          contenedorCategoria(),
-          const botonRegresar()),
-      //bottomNavigationBar: barraFooter(),
-    );
-  }
-}
-
-//El contenedor para todos los botones de las categorías
-Widget contenedorCategoria() {
-  return Container(
-    width: double.infinity,
-    decoration: const BoxDecoration(
-      color: Color(0xAAD2FEFF),
-      borderRadius: BorderRadius.only(
-        topLeft: Radius.circular(30),
-        topRight: Radius.circular(30),
+      //Color de fondo
+      backgroundColor: const Color(0xFF81F4DA),
+      //Barra superior
+      appBar: AppBar(
+        //Centra el título que está contenido dentro del appbar...
+        centerTitle: true,
+        //color de fondo del appbar
+        backgroundColor: const Color(0xAAFA94FD),
+        //Texto "Farmacias"
+        title: const Text("Categorías"),
       ),
-    ),
-    alignment: Alignment.center,
-    child: Column(
-      children: const <Widget>[
-        SizedBox(height: 15),
-        Text(
-          "Categorías",
-          style: TextStyle(
-            color: Colors.black,
-            fontWeight: FontWeight.bold,
-            fontSize: 26,
-          ),
-        ),
-        SizedBox(height: 20),
-        botonCategoria(nombreBoton: 'Gripe, tos y asma'),
-        SizedBox(height: 15),
-        botonCategoria(nombreBoton: 'Salud visual'),
-        SizedBox(height: 15),
-        botonCategoria(nombreBoton: 'Primeros auxilios'),
-        SizedBox(height: 15),
-        botonCategoria(nombreBoton: 'Nutrición y dieta'),
-        SizedBox(height: 15),
-        botonCategoria(nombreBoton: 'Piel'),
-        SizedBox(height: 15),
-        botonCategoria(nombreBoton: 'Vitaminas y minerales'),
-        SizedBox(height: 25),
-        //Intento de hacer un listview que NO utilice toda la pantalla.
-        /*Container(
-          child: ListView(
-            shrinkWrap: true,
-            children: <Widget>[
-              
-            ],
-          ),
-        ),*/
-      ],
-    ),
-  );
-}
 
-//Widget para la creación del botón de la categoría, al invocarlo, se debe brindar una cadena
-//con el nombre que tendrá el botón. Esto para que no se deba codificar un widget por cada botón.
+      //ESTO ES LO IMPORTANTE DEL CÓDIGO, AQUÍ SE MUESTRAN LOS REGISTROS DE LA COLECCIÓN FARMACIAS DIRECTO DE LA BASE DE DATOS
 
-class botonCategoria extends StatelessWidget {
-  const botonCategoria({Key? key, required this.nombreBoton}) : super(key: key);
-  final String nombreBoton;
+      //Se utiliza un constructor de una vista de listado, dentro de la cual, por medio del context, se crean elementos del mismo formato
+      //uno por cada elemento de la colección de la base de datos.
+      body: ListView.builder(
+        //La cantidad de farmacias
+        itemCount: docs.length,
+        //El elemento que se creará para cada farmacia, en este caso, un botón azul con text blanco.
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+            margin: const EdgeInsets.symmetric(horizontal: 25),
+            child: ButtonTheme(
+              minWidth: double.infinity,
+              height: 45.0,
+              child: FlatButton(
+                  color: Colors.blue[800],
 
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.symmetric(horizontal: 25),
-      child: ButtonTheme(
-        minWidth: double.infinity,
-        height: 45.0,
-        child: FlatButton(
-            color: Colors.blue[800],
-            //padding: EdgeInsets.symmetric(horizontal: 100, vertical: 15),
-            onPressed: () {
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => const CategoriaSeleccionada()));
-            },
-            child: Text(
-              nombreBoton,
-              style: const TextStyle(
-                fontSize: 25,
-                color: Colors.white,
-                fontFamily: "Tahoma",
-              ),
-            )),
-      ),
-    );
-  }
-}
-
-/*Widget botonCategoria2(String nombreBoton) {
-  return Container(
-    margin: const EdgeInsets.symmetric(horizontal: 25),
-    child: ButtonTheme(
-      minWidth: double.infinity,
-      height: 45.0,
-      child: FlatButton(
-          color: Colors.blue[800],
-          //padding: EdgeInsets.symmetric(horizontal: 100, vertical: 15),
-          onPressed: () {},
-          child: Text(
-            nombreBoton,
-            style: const TextStyle(
-              fontSize: 25,
-              color: Colors.white,
-              fontFamily: "Tahoma",
+                  //Esta función permite al botón realizar una acción, por ahora NO está definida la acción, así que si ve
+                  //otras lineas comentadas, no las borre por favor.
+                  onPressed: () {
+                    //Navigator.push(context,
+                    //MaterialPageRoute(builder: (context) => const Sucursales()));
+                  },
+                  child: Text(
+                    docs[index]['nombre'],
+                    style: const TextStyle(
+                      fontSize: 25,
+                      color: Colors.white,
+                      fontFamily: "Tahoma",
+                    ),
+                  )),
             ),
-          )),
-    ),
-  );
-}*/
-
-//Método para el botón "Regresar" más actualizado
-class botonRegresar extends StatelessWidget {
-  const botonRegresar({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.symmetric(horizontal: 100),
-      child: ButtonTheme(
-          minWidth: double.infinity,
-          height: 45.0,
-          child: FlatButton(
-              color: Colors.blue,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(18.0),
-              ),
-              //padding: EdgeInsets.symmetric(horizontal: 100, vertical: 15),
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: const Text(
-                "Regresar",
-                style: TextStyle(
-                  fontSize: 25,
-                  color: Colors.white,
-                  fontFamily: "Verdana",
-                ),
-              ))),
+          );
+        },
+      ),
     );
   }
 }
-
-// Primer método del botón Regresar
-/*Widget botonRegresar2() {
-  // ignore: deprecated_member_use
-  return FlatButton(
-      color: Colors.blue,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(18.0),
-      ),
-      padding: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
-      onPressed: () {},
-      child: Text(
-        "Regresar",
-        style: TextStyle(
-          fontSize: 20,
-          color: Colors.white,
-          fontFamily: "Arial",
-        ),
-      ));
-}*/
-
-//Una barra de navegación en la parte inferior que por el momento no se utiliza
-/*BottomNavigationBar barraFooter() {
-  return BottomNavigationBar(
-    items: <BottomNavigationBarItem>[
-      BottomNavigationBarItem(
-          icon: Icon(Icons.arrow_back_outlined), title: Text("Regresar")),
-      BottomNavigationBarItem(icon: Icon(Icons.home), title: Text("Menu")),
-    ],
-  );
-}*/
--- medicinasyfarmacias2/lib/pantallas/registro.dart
+++ medicinasyfarmacias2/lib/pantallas/registro.dart
@@ -1,64 +1,84 @@
 // ignore_for_file: deprecated_member_use, camel_case_types
 
 import 'package:flutter/material.dart';
-import 'package:medicinasyfarmacias/pantallas/menu.dart';
+import 'package:medicinasyfarmacias/database/dbusuarios.dart';
+//import 'package:medicinasyfarmacias/pantallas/menu.dart';
 import '../funciones/funciones.dart';
 
-class Registro extends StatefulWidget {
+class Registro extends StatelessWidget {
   const Registro({Key? key}) : super(key: key);
 
   @override
-  _RegistroState createState() => _RegistroState();
-}
-
-class _RegistroState extends State<Registro> {
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: cuerpoP(),
     );
   }
 }
 
-Widget cuerpoP() {
-  return Container(
-    decoration: const BoxDecoration(
-      color: Color(0xFF90CAF9),
-    ),
-    child: Center(
-      child: ListView(
-        children: <Widget>[
-          const SizedBox(
-            height: 30,
-          ),
-          tituloPantalla("Registro"),
-          const SizedBox(
-            height: 15,
-          ),
-          campoIcono(),
-          const SizedBox(
-            height: 15,
-          ),
-          campoDatos(),
-          const SizedBox(
-            height: 15,
-          ),
-          contenedorDat(),
-          const SizedBox(
-            height: 25,
-          ),
-          const botonRegistrarse(),
-          const SizedBox(
-            height: 20,
-          ),
-          const botonCancelar(),
-          const SizedBox(
-            height: 30,
-          ),
-        ],
+late DatabaseUsuarios db;
+
+class cuerpoP extends StatefulWidget {
+  const cuerpoP({Key? key}) : super(key: key);
+
+  @override
+  State<cuerpoP> createState() => _cuerpoPState();
+}
+
+class _cuerpoPState extends State<cuerpoP> {
+  List docs = [];
+  initialise() {
+    db = DatabaseUsuarios();
+    db.initiliase();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    initialise();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        color: Color(0xFF90CAF9),
       ),
-    ),
-  );
+      child: Center(
+        child: ListView(
+          children: <Widget>[
+            const SizedBox(
+              height: 30,
+            ),
+            tituloPantalla("Registro"),
+            const SizedBox(
+              height: 15,
+            ),
+            campoIcono(),
+            const SizedBox(
+              height: 15,
+            ),
+            campoDatos(),
+            const SizedBox(
+              height: 15,
+            ),
+            const contenedorDat(),
+            const SizedBox(
+              height: 25,
+            ),
+            const botonRegistrarse(),
+            const SizedBox(
+              height: 20,
+            ),
+            const botonCancelar(),
+            const SizedBox(
+              height: 30,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
 
 //campo del icono
@@ -79,47 +99,122 @@ Widget campoDatos() {
   );
 }
 
-//El contenedor
-Widget contenedorDat() {
-  return Container(
-    width: double.infinity,
-    decoration: const BoxDecoration(
-      color: Color(0xFFE1F5FE),
-      borderRadius: BorderRadius.only(
-        topLeft: Radius.circular(30),
-        topRight: Radius.circular(30),
-      ),
-    ),
-    alignment: Alignment.center,
-    child: Column(
-      children: <Widget>[
-        const SizedBox(height: 15),
-        campfield("Nombre"),
-        const SizedBox(height: 15),
-        campfield("Correo Electronico"),
-        const SizedBox(height: 15),
-        campfield("Nombre de Usuario"),
-        const SizedBox(height: 15),
-        campfield("Contraseña"),
-        const SizedBox(height: 15),
-        campfield("Confirme Contraseña"),
-        const SizedBox(height: 25),
-      ],
-    ),
-  );
-}
+final _fromKey = GlobalKey<FormState>();
+final nombreController = TextEditingController();
+final apellidoController = TextEditingController();
+final usuarioController = TextEditingController();
+final correoController = TextEditingController();
+final contraseniaController = TextEditingController();
 
-Widget campfield(String nombreText) {
-  return Container(
-    margin: const EdgeInsets.symmetric(horizontal: 25),
-    child: TextField(
-      decoration: InputDecoration(
-        fillColor: Colors.white,
-        filled: true,
-        hintText: nombreText,
+class contenedorDat extends StatelessWidget {
+  const contenedorDat({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      decoration: const BoxDecoration(
+        color: Color(0xFFE1F5FE),
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(30),
+          topRight: Radius.circular(30),
+        ),
       ),
-    ),
-  );
+      alignment: Alignment.center,
+      child: Form(
+        key: _fromKey,
+        child: Column(
+          children: <Widget>[
+            const SizedBox(height: 15),
+
+            // Nombre
+            TextFormField(
+              controller: nombreController,
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return "Debe colocar un nombre";
+                }
+                return null;
+              },
+              decoration: const InputDecoration(
+                fillColor: Colors.white,
+                filled: true,
+                hintText: "Nombre",
+              ),
+            ),
+            const SizedBox(height: 15),
+
+            // Apellido
+            TextFormField(
+              controller: apellidoController,
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return "Debe colocar un apellido";
+                }
+                return null;
+              },
+              decoration: const InputDecoration(
+                fillColor: Colors.white,
+                filled: true,
+                hintText: "Apellido",
+              ),
+            ),
+            const SizedBox(height: 15),
+
+            // Usuario
+            TextFormField(
+              controller: usuarioController,
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return "Debe colocar un usuario";
+                }
+                return null;
+              },
+              decoration: const InputDecoration(
+                fillColor: Colors.white,
+                filled: true,
+                hintText: "Usuario",
+              ),
+            ),
+            const SizedBox(height: 15),
+
+            // Correo electrónico
+            TextFormField(
+              controller: correoController,
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return "Debe colocar un correo";
+                }
+                return null;
+              },
+              decoration: const InputDecoration(
+                fillColor: Colors.white,
+                filled: true,
+                hintText: "Correo electrónico",
+              ),
+            ),
+            const SizedBox(height: 15),
+            // Contraseña
+            TextFormField(
+              controller: contraseniaController,
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return "Debe colocar una contraseña";
+                }
+                return null;
+              },
+              decoration: const InputDecoration(
+                fillColor: Colors.white,
+                filled: true,
+                hintText: "Contraseña",
+              ),
+            ),
+            const SizedBox(height: 25),
+          ],
+        ),
+      ),
+    );
+  }
 }
 
 class botonRegistrarse extends StatelessWidget {
@@ -139,8 +234,21 @@ class botonRegistrarse extends StatelessWidget {
             ),
             //padding: EdgeInsets.symmetric(horizontal: 100, vertical: 15),
             onPressed: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => const MenuApp()));
+              if (_fromKey.currentState!.validate()) {
+                db.create(
+                  nombreController.text,
+                  apellidoController.text,
+                  usuarioController.text,
+                  correoController.text,
+                  contraseniaController.text,
+                );
+                Navigator.pop(context, true);
+                showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return const Dialogo("Usuario registrado exitosamente");
+                    });
+              }
             },
             child: const Text(
               "Registrarse",
@@ -155,31 +263,27 @@ class botonRegistrarse extends StatelessWidget {
   }
 }
 
-//Método para el botón Registrarse
-/*Widget botonRegistrarse2() {
-  return Container(
-    margin: const EdgeInsets.symmetric(horizontal: 100),
-    child: ButtonTheme(
-      minWidth: double.infinity,
-      height: 45.0,
-      child: FlatButton(
-          color: Colors.blue,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(18.0),
-          ),
-          //padding: EdgeInsets.symmetric(horizontal: 100, vertical: 15),
-          onPressed: () {},
-          child: const Text(
-            "Registrarse",
-            style: TextStyle(
-              fontSize: 20,
-              color: Colors.white,
-              fontFamily: "Tahoma",
-            ),
-          )),
-    ),
-  );
-}*/
+class Dialogo extends StatelessWidget {
+  // ignore: prefer_typing_uninitialized_variables
+  final title;
+  // ignore: use_key_in_widget_constructors
+  const Dialogo(this.title);
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: const Text("Aviso"),
+      content: Text(title),
+      actions: [
+        FlatButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: const Text("OK"))
+      ],
+    );
+  }
+}
 
 class botonCancelar extends StatelessWidget {
   const botonCancelar({Key? key}) : super(key: key);
@@ -212,29 +316,3 @@ class botonCancelar extends StatelessWidget {
     );
   }
 }
-
-//Método para el botón Cancelar
-/*Widget botonCancelar2() {
-  return Container(
-    margin: const EdgeInsets.symmetric(horizontal: 100),
-    child: ButtonTheme(
-      minWidth: double.infinity,
-      height: 45.0,
-      child: FlatButton(
-          color: Colors.blue,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(18.0),
-          ),
-          //padding: EdgeInsets.symmetric(horizontal: 100, vertical: 15),
-          onPressed: () {},
-          child: const Text(
-            "Cancelar",
-            style: TextStyle(
-              fontSize: 20,
-              color: Colors.white,
-              fontFamily: "Tahoma",
-            ),
-          )),
-    ),
-  );
-}*/
